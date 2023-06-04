ab365e43ab450b51c9eece57fc4472785d835167
Jesse Peirce <jbp61116@gmail.com>
2023-06-03 22:57:03 -0500

    Add Typescript support in Ember

---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+// Types for compiled templates
+declare module 'multiattack-5e/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
+
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
