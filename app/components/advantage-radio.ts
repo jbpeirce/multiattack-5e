27acb284f3ab27cb49ba19ab0d2a8a79d0aa53ca
Jesse Peirce <jbp61116@gmail.com>
2024-01-18 11:24:29 -0600

    Add default checked value to advantage radio
    Ember's Input component does not support radio buttons properly.
    However, a standard HTML input, styled as a radio button, works
    equally well.

--- app/components/advantage-radio.ts
+++ app/components/advantage-radio.ts
@@ -10,4 +10,17 @@ export default class AdvantageRadioComponent extends Component {
     AdvantageState.STRAIGHT,
     AdvantageState.DISADVANTAGE,
   ];
+
+  /**
+   * Indicate whether this state should have its radio button checked by default
+   * @param the name of an advantage state labeling a radio button
+   * @return whether this state should be checked in the UI's radio button by
+   * default
+   */
+  getChecked = (stateName: string) => {
+    if (stateName == AdvantageState.STRAIGHT.name) {
+      return true;
+    }
+    return false;
+  };
 }
