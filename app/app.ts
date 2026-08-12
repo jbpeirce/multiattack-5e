6ffc20f46bc0caca0017811db1197ee22228c800
Jesse <jbp61116@gmail.com>
2026-08-11 22:23:02 -0500

    v5.4.1...v6.7.0
    Update ember to v6.7.0 using ember-cli-update (as updating past
    this point requires manual intervention).

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,11 @@ import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
 import config from 'multiattack-5e/config/environment';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
